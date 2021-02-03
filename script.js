5acbfc231668d44feb66a2ff69d6949107768198
zakir a siddiqi <zakir.siddiqi@gmail.com>
2021-02-03 15:02:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
     r = document.getElementById("rate").value;
-    
-    
     y = document.getElementById("years").value;
     a=p*(r*(1/100)*y;
-    document.getElementById("result").value=a;
-    
+    document.getElementById("demoa").value=r;
 }
         
