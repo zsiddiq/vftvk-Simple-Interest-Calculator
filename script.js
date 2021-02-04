14f12c1543eeb674266903d0294d72f7ac0c07af
zakir a siddiqi <zakir.siddiqi@gmail.com>
2021-02-03 20:23:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function compute()
-{
+{var p,r,y;
     p = Number(document.getElementById("principal").value);
     r = Number(document.getElementById("irate").value);
     y = Number(document.getElementById("years").value);
