f63b537d431e37d7dc809112a2b1ec511035ac4e
zakir a siddiqi <zakir.siddiqi@gmail.com>
2021-02-03 21:11:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ function compute()
         text = "Amount Input is valid";
     }
     document.getElementById("demoa").innerHTML=text;
+ 
     r=Number(document.getElementById("irate").value);
+ if (isNaN(r) || r < 1) {
+        text = "Rate Input is not valid";
+    }else{
+        text = "Rate Input is valid";
+    }
+    document.getElementById("demob").innerHTML=text;
+ 
     y=Number(document.getElementById("years").value);
     d=p*r*y
    document.getElementById("answer").value=d;
