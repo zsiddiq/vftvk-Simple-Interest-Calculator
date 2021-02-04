2211f8100536b7aba0a7e42f6238a4b0d9a3407f
zakir a siddiqi <zakir.siddiqi@gmail.com>
2021-02-03 21:15:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function compute()
-{var p,r,y,text,d;
+{var p,r,y,texta, textb,d;
     p=Number(document.getElementById("principal").value);
     if (isNaN(p) || p < 1) {
-        text = "Amount Input is not valid";
+        texta = "Amount Input is not valid";
     }else{
-        text = "Amount Input is valid";
+        texta = "Amount Input is valid";
     }
-    document.getElementById("demoa").innerHTML=text;
+    document.getElementById("demoa").innerHTML=texta;
  
     r=Number(document.getElementById("irate").value);
  if (isNaN(r) || r < 1) {
-        text = "Rate Input is not valid";
+        textb = "Rate Input is not valid";
     }else{
-        text = "Rate Input is valid";
+        textb = "Rate Input is valid";
     }
-    document.getElementById("demob").innerHTML=text;
+    document.getElementById("demob").innerHTML=textb;
  
     y=Number(document.getElementById("years").value);
     d=p*r*y
