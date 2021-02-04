d37bc7ab17df668d3bb8ad035445f1ac0840ec2d
zakir a siddiqi <zakir.siddiqi@gmail.com>
2021-02-04 15:17:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function compute()
     document.getElementById("demob").innerHTML=textb;
  
     y=Number(document.getElementById("years").value);
-    d=p*(r/100)*y
+    d=p*r*y
     document.getElementById("answer").value=d;
     document.getElementById("line1").innerHTML=line1c;
  
