f89505165c50367709d93f200106eb520cbf0d9b
zakir a siddiqi <zakir.siddiqi@gmail.com>
2021-02-05 07:55:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function compute()
 {var p,po,r,ro,y,texta, textb,d, line1c, line2c, line3c, line4c;
  line1c = "If you deposit ";
+ line2c = "at an interest rate of ";
     p=Number(document.getElementById("principal").value);
     if (isNaN(p) || p < 1) {
         texta = "Amount Input is not valid";
@@ -24,6 +25,7 @@ function compute()
     d=(p*(r/100.0)*y)
     document.getElementById("answer").value=d;
     document.getElementById("line1").innerHTML=line1c;
+ document.getElementById("line2").innerHTML=line2c;
     document.getElementById("pout").innerHTML=po;
  document.getElementById("rout").innerHTML=ro;
  
