d891227f932d3c4f3393b977eab7e8c4af7ea4dc
zakir a siddiqi <zakir.siddiqi@gmail.com>
2021-02-05 15:29:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function compute()
-{var n,p,po,r,ro,x,y,dout, texta, textb,d, line1c, line2c, line3c, line4c;
+{var n,p,po,r,ro,x,y,yo,dout, texta, textb,textc,d, line1c, line2c, line3c, line4c;
  line1c = "If you deposit ";
  line2c = "at an interest rate of ";
  line3c = "You will receive an amount of ";
  line4c = "in the year ";
     p=Number(document.getElementById("principal").value);
     if (isNaN(p) || p < 1) {
-        texta = "Amount Input is not valid";
+        texta = "Amount Input must be positive";
     }else{
         texta = "Amount Input is valid";
         po = p;
@@ -16,7 +16,7 @@ function compute()
     r=document.getElementById("irate").innerHTML;
  //alert(r);
  if (isNaN(r) || r < 0.5) {
-        textb = "Rate Input is not valid";
+        textb = "Rate Input must be positive";
     }else{
         textb = "Rate Input is valid";
         ro = r;
@@ -24,6 +24,13 @@ function compute()
     document.getElementById("demob").innerHTML=textb;
  
     y=Number(document.getElementById("years").value);
+ if (isNaN(y) || y < 0.0) {
+        textc = "Year Input must be positive";
+    }else{
+        textc = "Year Input is valid";
+        yo = y;
+    }
+    document.getElementById("democ").innerHTML=textc;
     d=(p*(r/100.0)*y);
     dout = d.toFixed(2);
     //document.getElementById("answer").value=d;
