e8ba2e36ce09529ad9319211895850457e62335d
zakir a siddiqi <zakir.siddiqi@gmail.com>
2021-02-06 20:27:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -40,10 +40,12 @@ function compute()
  n = (x.getFullYear())+y;
  line4c = line4c + n;
  document.getElementById("line4").innerHTML=line4c;
+ if (isNaN(po)){ 
  line1ct= line1ct;
     document.getElementById("line1t").innerHTML=line1ct;
  line1cn= po + ",";
     document.getElementById("line1n").innerHTML=line1cn;
+ }
  line2c= line2c + ro + "%.";
  document.getElementById("line2").innerHTML=line2c;
  line3c = line3c + dout + ",";
